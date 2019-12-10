498cfd8afe256d443b706e1559c4338f8b687980
melia miller <meliajm@gmail.com>
2019-12-10 05:35:43 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+require 'pry'
+def reverse_each_word(sentence)
+  # binding.pry
+  array = []
+  a = sentence.split(" ")
+  a.collect do |word|
+    array.push(word.reverse)
+  end
+  array.join(" ")
+  # binding.pry
+end
